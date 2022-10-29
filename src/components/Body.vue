7ca428c75ca3454beb3d08a1fb51c84491aececb
pedrozorn <itt.pedrodev53@gmail.com>
2022-10-29 09:56:31 +0900

    add vuetity

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,5 +1,2 @@
 <template>
-    <header>部分的に動的</header>
-    <router-view>動的な内容</router-view>
-    <footer>部分的に動的</footer>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,23 @@
 <template>
-    <div>
-        <h1>{{ title }}</h1>
-        <p> {{ text }}</p>
-    </div>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
 </template>
 
 <script>
-
-export default ({
-    data() {
-        return {
-            title: "Header",
-            text: "ヘッダーテスト"
-        }
-    }
-})
-</script >
+export default {
+  data() {
+    return {
+      title: "Header",
+      text: "ヘッダーテスト",
+    };
+  },
+};
+</script>
 
 <style scoped lang="scss">
 div {
-  border: 1px solid red
+  border: 1px solid red;
 }
 </style>
