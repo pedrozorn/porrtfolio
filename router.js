b2bec3c9bb8d5697ad993f7c5f5d2c517e2b7e58
pedrozorn <itt.pedrodev53@gmail.com>
2022-11-03 16:25:43 +0900

    add AboutPage

--- router.js
+++ router.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "/src/components/Home.vue";
-import About from "/src/components/About.vue:";
-
+import About from "/src/components/About.vue";
 Vue.use(Router);
-
 export default new Router({
   routes: [
     {
